2fb8e92d8beba656cbbe3c23ec6eea3155921faa
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-25 22:43:02 -0400

    adds zero-fill option

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 'use strict';
 
+var repeat = require('repeat-string');
+
 module.exports = function (str) {
-  return (str === 'utc' || str === 'UTC')
+  var day = (str === 'utc' || str === 'UTC')
     ? new Date().getUTCDate()
     : new Date().getDate();
+
+  if (str === 'DD') {
+    return repeat('0', 2 - String(day).length) + day;
+  }
+  return day;
 };
--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 
 /* deps: mocha */
 var assert = require('assert');
+var repeat = require('repeat-string');
 var day = require('./');
 
 describe('day()', function () {
@@ -11,5 +12,12 @@ describe('day()', function () {
 
   it('should return the current UTC month day', function () {
     assert.equal(day('UTC'), new Date().getUTCDate(), true);
+  });
+
+  it('should zero-fill the current day number', function () {
+    assert.equal(typeof day('DD') === 'string', true);
+    var date = new Date().getDate();
+    var res = repeat('0', 2 - String(date).length) + date;
+    assert.equal(day('DD'), res, true);
   });
 });
